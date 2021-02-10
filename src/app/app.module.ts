43161341f30d41efe240aa124ad692196d5d4d8e
mikemarkovich <78623857+mikemarkovich@users.noreply.github.com>
2021-02-09 20:09:22 -0500

    header and scores work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ScoresComponent } from "./components/scores/scores.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ScoresComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/scores/scores.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "scores-component",
+  templateUrl: "./scores.component.html",
+  styleUrls: ["./scores.component.css"]
+})
+export class ScoresComponent {}
