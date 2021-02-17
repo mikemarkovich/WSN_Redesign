2cab067af3bf946bea9f3a330bf76dede269b231
mikemarkovich <78623857+mikemarkovich@users.noreply.github.com>
2021-02-16 21:48:10 -0500

    add nav toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = "CodeSandbox";
+export class AppComponent implements OnInit {
+  isOpen: boolean = false;
+  ngOnInit() {}
+
+  toggleNav() {
+    this.isOpen = !this.isOpen;
+    const body = document.getElementsByTagName("body")[0];
+    body.classList.toggle("scroll-locked");
+  }
 }
