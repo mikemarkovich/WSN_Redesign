b3f0a26fa7b503345f06c4bd7032210ca2c23351
mikemarkovich <78623857+mikemarkovich@users.noreply.github.com>
2021-02-23 00:27:10 -0500

    mobile nav and responsive nav work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AppComponent implements OnInit {
   isOpen: boolean = false;
+  isSchoolsActive: boolean = false;
+  isSportsActive: boolean = false;
   ngOnInit() {}
 
   toggleNav() {
+    this.isSchoolsActive = false;
+    this.isSportsActive = false;
     this.isOpen = !this.isOpen;
     const body = document.getElementsByTagName("body")[0];
     body.classList.toggle("scroll-locked");
